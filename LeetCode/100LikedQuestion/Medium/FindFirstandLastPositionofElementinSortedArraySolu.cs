87bb3b79f72524cf9e1c8ad603fe0bb25f87cdd4
Mani Shankar <mani.shankar@MediaKind.Com>
2020-05-21 23:16:42 +0530

    serach in roatated array

---
+++ LeetCode/100LikedQuestion/Medium/FindFirstandLastPositionofElementinSortedArraySolu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._100LikedQuestion.Medium
+{
+    class FindFirstandLastPositionofElementinSortedArraySolu : BaseClass
+    {
+        public override void Run()
+        {
+            base.Run();
+            var result = SearchRange(new int[] {1 }, 1);
+        }
+        int[] Nums;
+        public int[] SearchRange(int[] nums, int target)
+        {
+            Nums = nums;
+            int pos = Array.BinarySearch(nums, target);
+            if (pos<0 ) return new int[] { -1, -1 };
+            int j = pos, k = pos;
+            while (j< nums.Length-1 && nums[j + 1] == target)
+            {
+                j++;
+            }
+            while (k > 0 && nums[k-1] == target )
+            {
+                k--;
+            }
+            return new int[] { k, j };
+        }
+
+        public int BinarySerach(int start, int end, int target)
+        {
+            if (end < start) return -1;
+            if (end == start)
+            {
+                if (Nums[end] == target) return start;
+                return -1;
+            }
+            var mid = (start + end) / 2;
+            if (Nums[mid] == target) return mid;
+            if (Nums[mid] > Nums[start])
+            {
+                if (target >= Nums[start] && target <= Nums[mid - 1])
+                    return BinarySerach(start, mid - 1, target);
+                else
+                    return BinarySerach(mid + 1, end, target);
+            }
+            else
+            {
+                if (target >= Nums[mid + 1] && target <= Nums[end])
+                    return BinarySerach(mid + 1, end, target);
+                else
+                    return BinarySerach(start, mid - 1, target);
+            }
+        }
+    }
+}
---
+++ LeetCode/100LikedQuestion/Medium/SearchInRotatedSortedArraySolu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode._100LikedQuestion.Medium
+{
+    class SearchInRotatedSortedArraySolu : BaseClass
+    {
+        public override void Run()
+        {
+            base.Run();
+            var result = Search(new int[] {4,5,6,7,0,1,2}, 0);
+
+        }
+        int[] Nums;
+        public int Search(int[] nums, int target)
+        {
+            Nums = nums;
+
+            return BinarySerach(0, nums.Length-1, target);   
+        }
+        public int BinarySerach(int start, int end, int target)
+        {
+            if (end < start) return -1;
+            if (end == start)
+            {
+               if( Nums[end] ==target) return start;
+                return -1;
+            }
+            var mid = (start+ end )/ 2;
+            if (Nums[mid] == target) return mid;
+            if (Nums[mid] > Nums[start])
+            {
+                if (target >= Nums[start] && target <= Nums[mid - 1])
+                    return BinarySerach(start, mid - 1, target);
+                else
+                    return BinarySerach(mid + 1, end, target);
+            }
+            else
+            {
+                if (target >= Nums[mid + 1] && target <= Nums[end])
+                    return BinarySerach(mid + 1, end, target);
+                else
+                    return BinarySerach(start, mid - 1, target);
+            }
+        }
+    }
+}
